45e208cadc699cb4c1218b6cd41feae6c08e710b
mogluu <47318409+mogluu@users.noreply.github.com>
2020-03-05 14:25:05 -0500

    release first pygluu-kuberentes version, release 4.1

--- pygluu/kubernetes/__init__.py
+++ pygluu/kubernetes/__init__.py
@@ -1,10 +1,7 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 """
  License terms and conditions:
  https://www.gluu.org/license/enterprise-edition/
 """
 
-__version__ = "1.0.0_dev"
-__previous_version__ = "1.0.0_dev"
+__version__ = "1.0.0"
+__previous_version__ = "1.0.0_alpha"
