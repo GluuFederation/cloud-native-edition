26c22acbb5a35585a140cb50e54f3c96e91d131e
mogluu <47318409+mogluu@users.noreply.github.com>
2020-06-04 06:42:41 -0400

    Fix couchbase output yamls

--- pygluu/kubernetes/couchbase.py
+++ pygluu/kubernetes/couchbase.py
@@ -502,14 +502,14 @@ class Couchbase(object):
 
         self.create_couchbase_gluu_cert_pass_secrets(encoded_ca_crt_string, encoded_cb_pass_string)
 
-        command = "./{}/bin/cbopcfg > {}".format(self.couchbase_source_file, self.couchbase_operator_dac_file)
+        command = "./{}/bin/cbopcfg --namespace {} > {}".format(self.couchbase_source_file,
+                                                                self.settings["COUCHBASE_NAMESPACE"],
+                                                                self.couchbase_operator_dac_file)
         subprocess_cmd(command)
         couchbase_cluster_parser = Parser(self.couchbase_cluster_file, "CouchbaseCluster")
         couchbase_cluster_parser["spec"]["networking"]["tls"]["static"]["serverSecret"] = "couchbase-server-tls"
         couchbase_cluster_parser["spec"]["networking"]["tls"]["static"]["operatorSecret"] = "couchbase-operator-tls"
         couchbase_cluster_parser.dump_it()
-
-
 
         self.kubernetes.create_objects_from_dict(self.couchbase_custom_resource_definition_file,
                                                  namespace=cb_namespace)
