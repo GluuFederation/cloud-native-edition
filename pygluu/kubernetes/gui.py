130e50429d220918d4379274788305adfe0b323a
Riza Arsyi <riza.arsyi@gmail.com>
2020-06-23 14:48:00 +0700

    initial gui installer

---
+++ pygluu/kubernetes/gui.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+ License terms and conditions for Gluu Cloud Native Edition:
+ https://www.apache.org/licenses/LICENSE-2.0
+ GUI is an installation method of deploying Gluu.
+"""
