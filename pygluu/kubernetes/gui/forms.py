85c3ce1443828858d1c8e3baa1cd1f98a0bf9db2
Riza Arsyi <riza.arsyi@gmail.com>
2020-08-28 15:06:51 +0700

    update jackrabbit cluster mode

--- pygluu/kubernetes/gui/forms.py
+++ pygluu/kubernetes/gui/forms.py
@@ -274,19 +274,66 @@ class GluuGatewayForm(FlaskForm):
 
 
 class JackrabbitForm(FlaskForm):
-    install_jackrabbit = RadioField("Install Jackrabbit",
-                                    choices=[("Y", "Yes"), ("N", "No")],
-                                    default="Y",
-                                    validators=[DataRequired()])
+    """
+    Jackrabbit Form
+
+    Fields
+    ------
+        install_jackrabbit (string|required) : default Y
+        jackrabbit_url (string|required_if install_jackrabbit = N): default http://jackrabbit:8080
+        jackrabbit_admin_id (string|required): default admin
+        jackrabbit_admin_password (string|required): default auto generated
+        jackrabbit_admin_password_confirmation (string|required|equal to jackrabbit_admin_password)
+        jackrabbit_cluster (string|required): default Y
+        jackrabbit_pg_user (string|required_if jackrabbit_cluster = Y): default jackrabbit
+        jackrabbit_pg_password (string|required_if jackrabbit_cluster = Y): default auto generated
+        jackrabbit_pg_password_confirmation (string|required_if jackrabbit_cluster = Y| equal to jackrabbit_pg_password)
+        jackrabbit_pg_database (string|required_if jackrabbit_cluster = Y)
+        jackrabbit_storage_size (string|required_if install_jackrabbit = Y)
+    """
+    install_jackrabbit = RadioField(
+        "Install Jackrabbit content repository",
+        choices=[("Y", "Yes"), ("N", "No")],
+        default="Y",
+        validators=[DataRequired()])
     jackrabbit_url = StringField(
         "Please enter jackrabbit url",
         default="http://jackrabbit:8080",
         validators=[RequiredIfFieldEqualTo("install_jackrabbit", "N"),
                     URL(require_tld=False, message="Url format is wrong")])
-    jackrabbit_user = StringField(
-        "Please enter jackrabbit user",
+    jackrabbit_admin_id = StringField(
+        "Please enter jackrabbit admin user",
         default="admin",
-        validators=[RequiredIfFieldEqualTo("install_jackrabbit", "N")])
+        validators=[InputRequired()])
+    jackrabbit_admin_password = StringField(
+        "Jackrabbit Admin Password",
+        widget=PasswordInput(hide_value=False),
+        validators=[InputRequired(),
+                    password_requirement_check])
+    jackrabbit_admin_password_confirmation = StringField(
+        "Jackrabbit Admin Password Confirmation",
+        widget=PasswordInput(hide_value=False),
+        validators=[InputRequired(),
+                    EqualTo("jackrabbit_admin_password")])
+    jackrabbit_cluster = RadioField(
+        "Enable Jackrabbit in cluster mode[beta] Recommended in production",
+        choices=[("Y", "Yes"), ("N", "No")],
+        default="Y",
+        validators=[DataRequired()])
+    jackrabbit_pg_user = StringField("Please enter a user for jackrabbit postgres database",
+                                     default="jackrabbit",
+                                     validators=[RequiredIfFieldEqualTo("jackrabbit_cluster", "Y")])
+    jackrabbit_pg_password = StringField("Jackrabbit PG Password",
+                                         widget=PasswordInput(hide_value=False),
+                                         validators=[RequiredIfFieldEqualTo("jackrabbit_cluster", "Y"),
+                                                     password_requirement_check])
+    jackrabbit_pg_password_confirmation = StringField(
+        "Jackrabbit PG Password Confirmation",
+        widget=PasswordInput(hide_value=False),
+        validators=[RequiredIfFieldEqualTo("jackrabbit_cluster", "Y"),
+                    EqualTo("jackrabbit_pg_password")])
+    jackrabbit_database = StringField("Please enter jackrabbit postgres database name",
+                                      default="jackrabbit")
     jackrabbit_storage_size = StringField(
         "Size of Jackrabbit content repository volume storage",
         default="4Gi",
