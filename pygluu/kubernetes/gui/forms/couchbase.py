a5c39ba9498e76333e93328b5cfe21b6e3d01eaa
Riza Arsyi <riza.arsyi@gmail.com>
2020-09-29 11:26:12 +0700

    update couchbase gluu user credentials label

--- pygluu/kubernetes/gui/forms/couchbase.py
+++ pygluu/kubernetes/gui/forms/couchbase.py
@@ -89,17 +89,17 @@ class CouchbaseForm(FlaskForm):
         "Couchbase superuser password confirm",
         widget=PasswordInput(hide_value=False),
         validators=[InputRequired(), EqualTo("couchbase_superuser_password")])
-    couchbase_user = StringField("Please enter couchbase username",
-                                 default="admin",
+    couchbase_user = StringField("Please enter gluu couchbase username",
+                                 default="gluu",
                                  validators=[InputRequired()])
     couchbase_password = StringField(
-        "Couchbase password",
+        "Couchbase gluu user password",
         widget=PasswordInput(hide_value=False),
         validators=[InputRequired(), password_requirement_check()],
         description="Password is randomly generated with 6 characters contain "
                     "number, uppercase letter, lower case letter and symbol")
     couchbase_password_confirmation = StringField(
-        "Couchbase password confirm",
+        "Couchbase gluu user password confirm",
         widget=PasswordInput(hide_value=False),
         validators=[InputRequired(), EqualTo("couchbase_password")])
     couchbase_cn = StringField("Enter Couchbase certificate common name.",
