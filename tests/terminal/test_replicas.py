bef985dbfe1e581297cccbae1c9d684701051736
iromli <isman.firmansyah@gmail.com>
2020-10-08 00:03:41 +0700

    add more testcases

---
+++ tests/terminal/test_replicas.py
@@ -0,0 +1,138 @@
+import pytest
+
+
+def test_prompt_replicas_oxauth(monkeypatch, settings):
+    from pygluu.kubernetes.terminal.replicas import PromptReplicas
+
+    monkeypatch.setattr("click.prompt", lambda x, default: 1)
+
+    # bypass
+    settings.set("OXTRUST_REPLICAS", 1)
+
+    PromptReplicas(settings).prompt_replicas()
+    assert settings.get("OXAUTH_REPLICAS") == 1
+
+
+def test_prompt_replicas_oxtrust(monkeypatch, settings):
+    from pygluu.kubernetes.terminal.replicas import PromptReplicas
+
+    monkeypatch.setattr("click.prompt", lambda x, default: 1)
+
+    # bypass
+    settings.set("OXAUTH_REPLICAS", 1)
+
+    PromptReplicas(settings).prompt_replicas()
+    assert settings.get("OXTRUST_REPLICAS") == 1
+
+
+def test_prompt_replicas_fido2(monkeypatch, settings):
+    from pygluu.kubernetes.terminal.replicas import PromptReplicas
+
+    monkeypatch.setattr("click.prompt", lambda x, default: 1)
+
+    # bypass
+    settings.set("OXAUTH_REPLICAS", 1)
+    settings.set("OXTRUST_REPLICAS", 1)
+
+    settings.set("ENABLE_FIDO2", "Y")
+    PromptReplicas(settings).prompt_replicas()
+    assert settings.get("FIDO2_REPLICAS") == 1
+
+
+def test_prompt_replicas_scim(monkeypatch, settings):
+    from pygluu.kubernetes.terminal.replicas import PromptReplicas
+
+    monkeypatch.setattr("click.prompt", lambda x, default: 1)
+
+    # bypass
+    settings.set("OXAUTH_REPLICAS", 1)
+    settings.set("OXTRUST_REPLICAS", 1)
+
+    settings.set("ENABLE_SCIM", "Y")
+    PromptReplicas(settings).prompt_replicas()
+    assert settings.get("SCIM_REPLICAS") == 1
+
+
+@pytest.mark.parametrize("type_", ["ldap", "hybrid"])
+def test_prompt_replicas_persistence(monkeypatch, settings, type_):
+    from pygluu.kubernetes.terminal.replicas import PromptReplicas
+
+    monkeypatch.setattr("click.prompt", lambda x, default: 1)
+
+    # bypass
+    settings.set("OXAUTH_REPLICAS", 1)
+    settings.set("OXTRUST_REPLICAS", 1)
+
+    settings.set("PERSISTENCE_BACKEND", type_)
+    PromptReplicas(settings).prompt_replicas()
+    assert settings.get("LDAP_REPLICAS") == 1
+
+
+def test_prompt_replicas_oxshibboleth(monkeypatch, settings):
+    from pygluu.kubernetes.terminal.replicas import PromptReplicas
+
+    monkeypatch.setattr("click.prompt", lambda x, default: 1)
+
+    # bypass
+    settings.set("OXAUTH_REPLICAS", 1)
+    settings.set("OXTRUST_REPLICAS", 1)
+
+    settings.set("ENABLE_OXSHIBBOLETH", "Y")
+    PromptReplicas(settings).prompt_replicas()
+    assert settings.get("OXSHIBBOLETH_REPLICAS") == 1
+
+
+def test_prompt_replicas_oxpassport(monkeypatch, settings):
+    from pygluu.kubernetes.terminal.replicas import PromptReplicas
+
+    monkeypatch.setattr("click.prompt", lambda x, default: 1)
+
+    # bypass
+    settings.set("OXAUTH_REPLICAS", 1)
+    settings.set("OXTRUST_REPLICAS", 1)
+
+    settings.set("ENABLE_OXPASSPORT", "Y")
+    PromptReplicas(settings).prompt_replicas()
+    assert settings.get("OXPASSPORT_REPLICAS") == 1
+
+
+def test_prompt_replicas_oxd(monkeypatch, settings):
+    from pygluu.kubernetes.terminal.replicas import PromptReplicas
+
+    monkeypatch.setattr("click.prompt", lambda x, default: 1)
+
+    # bypass
+    settings.set("OXAUTH_REPLICAS", 1)
+    settings.set("OXTRUST_REPLICAS", 1)
+
+    settings.set("ENABLE_OXD", "Y")
+    PromptReplicas(settings).prompt_replicas()
+    assert settings.get("OXD_SERVER_REPLICAS") == 1
+
+
+def test_prompt_replicas_casa(monkeypatch, settings):
+    from pygluu.kubernetes.terminal.replicas import PromptReplicas
+
+    monkeypatch.setattr("click.prompt", lambda x, default: 1)
+
+    # bypass
+    settings.set("OXAUTH_REPLICAS", 1)
+    settings.set("OXTRUST_REPLICAS", 1)
+
+    settings.set("ENABLE_CASA", "Y")
+    PromptReplicas(settings).prompt_replicas()
+    assert settings.get("CASA_REPLICAS") == 1
+
+
+def test_prompt_replicas_radius(monkeypatch, settings):
+    from pygluu.kubernetes.terminal.replicas import PromptReplicas
+
+    monkeypatch.setattr("click.prompt", lambda x, default: 1)
+
+    # bypass
+    settings.set("OXAUTH_REPLICAS", 1)
+    settings.set("OXTRUST_REPLICAS", 1)
+
+    settings.set("ENABLE_RADIUS", "Y")
+    PromptReplicas(settings).prompt_replicas()
+    assert settings.get("RADIUS_REPLICAS") == 1
--- tests/terminal/test_volumes.py
+++ tests/terminal/test_volumes.py
@@ -8,9 +8,7 @@ def test_volumes_identifier(monkeypatch, settings, persistence):
     monkeypatch.setattr("click.prompt", lambda x: "vol-1234")
 
     settings.set("PERSISTENCE_BACKEND", persistence)
-
-    prompt = PromptVolumes(settings)
-    prompt.prompt_volumes_identifier()
+    PromptVolumes(settings).prompt_volumes_identifier()
     assert settings.get("LDAP_STATIC_VOLUME_ID") == "vol-1234"
 
 
@@ -21,7 +19,69 @@ def test_disk_uris(monkeypatch, settings, persistence):
     monkeypatch.setattr("click.prompt", lambda x: "MC_aks")
 
     settings.set("PERSISTENCE_BACKEND", persistence)
-
-    prompt = PromptVolumes(settings)
-    prompt.prompt_disk_uris()
+    PromptVolumes(settings).prompt_disk_uris()
     assert settings.get("LDAP_STATIC_DISK_URI") == "MC_aks"
+
+
+@pytest.mark.parametrize("arch, vol_type", [
+    ("eks", 7),
+    ("gke", 12),
+    ("aks", 17),
+    ("do", 22),
+    ("local", 26),
+])
+def test_prompt_app_volume_type(monkeypatch, settings, arch, vol_type):
+    from pygluu.kubernetes.terminal.volumes import PromptVolumes
+
+    monkeypatch.setattr("click.prompt", lambda x, default: vol_type)
+
+    settings.set("DEPLOYMENT_ARCH", arch)
+    PromptVolumes(settings).prompt_app_volume_type()
+    assert settings.get("APP_VOLUME_TYPE") == vol_type
+
+
+@pytest.mark.parametrize("persistence", ["ldap", "hybrid"])
+def test_prompt_storage(monkeypatch, settings, persistence):
+    from pygluu.kubernetes.terminal.volumes import PromptVolumes
+
+    monkeypatch.setattr("click.prompt", lambda x, default: "4Gi")
+
+    settings.set("PERSISTENCE_BACKEND", persistence)
+    PromptVolumes(settings).prompt_storage()
+    assert settings.get("LDAP_STORAGE_SIZE") == "4Gi"
+
+
+def test_prompt_volumes_microk8s(settings):
+    from pygluu.kubernetes.terminal.volumes import PromptVolumes
+
+    settings.set("DEPLOYMENT_ARCH", "microk8s")
+    PromptVolumes(settings).prompt_volumes()
+    assert settings.get("APP_VOLUME_TYPE") == 1
+
+
+def test_prompt_volumes_minikube(settings):
+    from pygluu.kubernetes.terminal.volumes import PromptVolumes
+
+    settings.set("DEPLOYMENT_ARCH", "minikube")
+    PromptVolumes(settings).prompt_volumes()
+    assert settings.get("APP_VOLUME_TYPE") == 2
+
+
+@pytest.mark.parametrize("vol_type", [0, 8, 13, 18])
+def test_prompt_volumes_misc_vol_type(settings, vol_type):
+    from pygluu.kubernetes.terminal.volumes import PromptVolumes
+
+    settings.set("APP_VOLUME_TYPE", vol_type)
+    PromptVolumes(settings).prompt_volumes()
+
+
+@pytest.mark.parametrize("arch", ["aks", "eks", "gke"])
+def test_prompt_volumes_jackrabbit(monkeypatch, settings, arch):
+    from pygluu.kubernetes.terminal.volumes import PromptVolumes
+
+    monkeypatch.setattr("click.prompt", lambda x, default: "io1")
+
+    settings.set("DEPLOYMENT_ARCH", arch)
+    settings.set("APP_VOLUME_TYPE", 7)
+    PromptVolumes(settings).prompt_volumes()
+    assert settings.get("LDAP_JACKRABBIT_VOLUME") == "io1"
