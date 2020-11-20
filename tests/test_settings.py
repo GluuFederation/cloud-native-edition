ef9afb10640db3b7631ff3ae57ecfd7a154daebc
riza arsyi <rizarsyi@users.noreply.github.com>
2020-11-20 13:48:22 +0700

    test(test_settings): Improve test cases for settings module (#260)
    * test(test_settings): add test case checking settings file
    
    * refactor(settings-handler): optimize settings load and validation
    
    - Optimize settings validation logic in order to make testing easier
    - add error handling on loading settings and schema files.
    
    * test(test_settings): add test coverage on validate settings
    
    * style(settings): remove unnecessary code

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,4 +1,14 @@
 import logging
+import json
+from pathlib import Path
+
+
+def test_load_invalid_settings(tmpdir, settings):
+    p = Path(tmpdir) / 'test_settings.json'
+    settings.setting_file = p
+    p.write_text('')
+    settings.load()
+    assert "Non valid settings.json" in settings.errors[0]
 
 
 def test_store_data_exception(caplog, settings):
@@ -46,3 +56,56 @@ def test_reset_data_exception(caplog, monkeypatch, settings):
     with caplog.at_level(logging.INFO):
         assert settings.reset_data() is False
         assert "Uncaught error" in caplog.text
+
+
+def test_settings_is_not_exist(settings, tmpdir):
+    p = Path(tmpdir) / 'test_settings.json'
+    settings.setting_file = p
+
+    assert settings.is_exist() is False
+
+
+def test_settings_is_exist(settings, tmpdir):
+    p = Path(tmpdir) / 'test_settings.json'
+    p.write_text('{}')
+    settings.setting_file = p
+
+    assert settings.is_exist() is True
+
+
+def test_settings_validation_is_valid(settings, tmpdir):
+
+    settings.set("ACCEPT_CN_LICENSE", "Y")
+    settings_object = json.dumps(settings.db)
+    p = Path(tmpdir) / 'test_settings.json'
+    p.write_text(settings_object)
+    settings.setting_file = p
+
+    assert settings.validate() is True
+
+
+def test_settings_validation_is_invalid(settings, tmpdir):
+
+    settings.set("ACCEPT_CN_LICENSE", "true")
+    settings.set("ADMIN_PW", "123123123")
+    settings.set("INSTALL_GLUU_GATEWAY", "Y")
+    settings.db.pop("KONG_NAMESPACE")
+    settings_object = json.dumps(settings.db)
+    p = Path(tmpdir) / 'test_settings.json'
+    p.write_text(settings_object)
+    settings.setting_file = p
+
+    assert settings.validate() is False
+
+
+def test_settings_validation_not_exist_setting(settings, tmpdir):
+    p = Path(tmpdir) / 'test_settings.json'
+    settings.setting_file = p
+    assert settings.validate() is True
+
+
+def test_settings_validation_not_valid_setting(settings, tmpdir):
+    p = Path(tmpdir) / 'test_settings.json'
+    settings.setting_file = p
+    p.write_text('')
+    assert settings.validate() is False
