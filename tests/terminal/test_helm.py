94e2ca8fc26218950fd227f3131bb7baace80b1f
moabu <47318409+moabu@users.noreply.github.com>
2021-01-13 06:48:53 -0500

    test(multi-cluster): test-cases
    #275

--- tests/terminal/test_helm.py
+++ tests/terminal/test_helm.py
@@ -13,6 +13,7 @@ def test_helm_release_name(monkeypatch, settings, given, expected):
     settings.set("NGINX_INGRESS_RELEASE_NAME", "ningress")
     settings.set("NGINX_INGRESS_NAMESPACE", "ingress-nginx")
     settings.set("INSTALL_GLUU_GATEWAY", "N")
+    settings.set("CN_LDAP_MULTI_CLUSTER", "N")
 
     prompt = PromptHelm(settings)
     prompt.prompt_helm()
@@ -31,6 +32,7 @@ def test_helm_ingress_release_name(monkeypatch, settings, given, expected):
     settings.set("CN_HELM_RELEASE_NAME", "gluu")
     settings.set("NGINX_INGRESS_NAMESPACE", "ingress-nginx")
     settings.set("INSTALL_GLUU_GATEWAY", "N")
+    settings.set("CN_LDAP_MULTI_CLUSTER", "N")
 
     prompt = PromptHelm(settings)
     prompt.prompt_helm()
@@ -49,6 +51,7 @@ def test_helm_ingress_namespace(monkeypatch, settings, given, expected):
     settings.set("NGINX_INGRESS_RELEASE_NAME", "ningress")
     settings.set("CN_HELM_RELEASE_NAME", "gluu")
     settings.set("INSTALL_GLUU_GATEWAY", "N")
+    settings.set("CN_LDAP_MULTI_CLUSTER", "N")
 
     prompt = PromptHelm(settings)
     prompt.prompt_helm()
@@ -66,6 +69,7 @@ def test_helm_gg_helm_release_name(monkeypatch, settings, given, expected):
 
     settings.set("NGINX_INGRESS_RELEASE_NAME", "ningress")
     settings.set("NGINX_INGRESS_NAMESPACE", "ingress-nginx")
+    settings.set("CN_LDAP_MULTI_CLUSTER", "N")
     settings.set("INSTALL_GLUU_GATEWAY", "Y")
     settings.set("GLUU_GATEWAY_UI_HELM_RELEASE_NAME", "gluu-gateway-ui")
 
@@ -86,8 +90,39 @@ def test_helm_gg_ui_helm_release_name(monkeypatch, settings, given, expected):
     settings.set("NGINX_INGRESS_RELEASE_NAME", "ningress")
     settings.set("NGINX_INGRESS_NAMESPACE", "ingress-nginx")
     settings.set("INSTALL_GLUU_GATEWAY", "Y")
+    settings.set("CN_LDAP_MULTI_CLUSTER", "N")
     settings.set("KONG_HELM_RELEASE_NAME", "gluu-gateway")
 
     prompt = PromptHelm(settings)
     prompt.prompt_helm()
     assert settings.get("GLUU_GATEWAY_UI_HELM_RELEASE_NAME") == expected
+
+
+def test_helm_ldap_multi_cluster_name(monkeypatch, settings):
+    from pygluu.kubernetes.terminal.helm import PromptHelm
+
+    monkeypatch.setattr("click.confirm", lambda x, default: True)
+
+    settings.set("CN_HELM_RELEASE_NAME", "gluu")
+    settings.set("NGINX_INGRESS_RELEASE_NAME", "ningress")
+    settings.set("NGINX_INGRESS_NAMESPACE", "ingress-nginx")
+    settings.set("INSTALL_GLUU_GATEWAY", "N")
+    settings.set("CN_LDAP_MULTI_CLUSTER", "Y")
+    settings.set("CN_LDAP_SERF_PORT", "30946")
+    settings.set("CN_LDAP_ADVERTISE_ADDRESS", "demoexample.gluu.org:30946")
+    settings.set("CN_LDAP_ADVERTISE_ADMIN_PORT", "30444")
+    settings.set("CN_LDAP_ADVERTISE_LDAPS_PORT", "30636")
+    settings.set("CN_LDAP_ADVERTISE_REPLICATION_PORT", "30989")
+    settings.set("CN_LDAP_SECONDARY_CLUSTER", "Y")
+    settings.set("CN_LDAP_SERF_PEERS", "[firstldap.gluu.org:30946]")
+    prompt = PromptHelm(settings)
+    prompt.prompt_helm()
+    assert settings.get("CN_LDAP_MULTI_CLUSTER") == "Y"
+    assert settings.get("CN_LDAP_SERF_PORT") == "30946"
+    assert settings.get("CN_LDAP_ADVERTISE_ADDRESS") == "demoexample.gluu.org:30946"
+    assert settings.get("CN_LDAP_ADVERTISE_ADMIN_PORT") == "30444"
+    assert settings.get("CN_LDAP_ADVERTISE_LDAPS_PORT") == "30636"
+    assert settings.get("CN_LDAP_ADVERTISE_REPLICATION_PORT") == "30989"
+    assert settings.get("CN_LDAP_SECONDARY_CLUSTER") == "Y"
+    assert settings.get("CN_LDAP_SERF_PEERS") == "[firstldap.gluu.org:30946]"
+
