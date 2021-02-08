ca1a0d8d129286c31bf9304638cae076e483b3f9
moabu <47318409+moabu@users.noreply.github.com>
2021-02-08 03:08:59 -0500

    feat(realease): release 1.4.0
    Updated images to 4.2.3_01

--- pygluu/kubernetes/__init__.py
+++ pygluu/kubernetes/__init__.py
@@ -3,6 +3,6 @@
  https://www.apache.org/licenses/LICENSE-2.0
 """
 
-__version__ = "1.3.3"
-__previous_version__ = "1.3.2"
+__version__ = "1.4.0"
+__previous_version__ = "1.3.3"
 
--- tests/terminal/test_version.py
+++ tests/terminal/test_version.py
@@ -13,15 +13,15 @@ def test_version_no_prompt(settings):
 
 
 @pytest.mark.parametrize("given, expected", [
-    ("", "4.2.2_02"),  # default if empty
-    ("4.2.2_dev", "4.2.2_dev"),  # non-empty shouldn't be overriden
+    ("", "4.2.3_01"),  # default if empty
+    ("4.2.3_dev", "4.2.3_dev"),  # non-empty shouldn't be overriden
 ])
 def test_version_merge_names_tags(settings, given, expected):
     import json
     from pygluu.kubernetes.terminal.version import PromptVersion
 
     with open("./gluu_versions.json", "w") as f:
-        json.dump({"4.2": {"LDAP_IMAGE_TAG": "4.2.2_02"}}, f)
+        json.dump({"4.2": {"LDAP_IMAGE_TAG": "4.2.3_01"}}, f)
 
     settings.set("GLUU_VERSION", "4.2")
     settings.set("LDAP_IMAGE_TAG", given)
