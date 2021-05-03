5b969f44b0dd8cfed1de6afb293f9b8954e378f9
moabu <47318409+moabu@users.noreply.github.com>
2021-05-03 03:48:54 -0400

    ci: fix tests

--- tests/terminal/test_postgres.py
+++ tests/terminal/test_postgres.py
@@ -10,29 +10,11 @@ def test_postgres_namespace(monkeypatch, settings, given, expected):
 
     monkeypatch.setattr("click.prompt", lambda x, default: given or expected)
     settings.set("INSTALL_POSTGRES", "Y")
-    settings.set("POSTGRES_REPLICAS", 3)
     settings.set("POSTGRES_URL", "postgres.postgres.svc.cluster.local")
 
     prompt = PromptPostgres(settings)
     prompt.prompt_postgres()
     assert settings.get("POSTGRES_NAMESPACE") == expected
-
-
-@pytest.mark.parametrize("given, expected", [
-    ("", 3),  # default
-    (2, 2),
-])
-def test_postgres_replicas(monkeypatch, settings, given, expected):
-    from pygluu.kubernetes.terminal.postgres import PromptPostgres
-    monkeypatch.setattr("click.prompt", lambda x, default: given or expected)
-    settings.set("INSTALL_POSTGRES", "Y")
-    settings.set("POSTGRES_NAMESPACE", "postgres")
-    settings.set("POSTGRES_URL", "postgres.postgres.svc.cluster.local")
-
-    prompt = PromptPostgres(settings)
-    prompt.prompt_postgres()
-    assert settings.get("POSTGRES_REPLICAS") == expected
-
 
 @pytest.mark.parametrize("given, expected", [
     ("", "postgres.postgres.svc.cluster.local"),  # default
