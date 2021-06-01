0cab63162d74bd3276d2dcf7bc186b0bc5019c96
moabu <47318409+moabu@users.noreply.github.com>
2021-06-01 07:21:28 -0400

    fix: update versions for dev installations

--- pygluu/kubernetes/terminal/google.py
+++ pygluu/kubernetes/terminal/google.py
@@ -34,7 +34,8 @@ class PromptGoogle:
                 self.settings.set("GOOGLE_SPANNER_DATABASE_ID",
                                   click.prompt("Please enter the google spanner database ID",
                                                default=""))
-
+        # Feature not implemented yet
+        self.settings.set("USE_GOOGLE_SECRET_MANAGER", "N")
         if not self.settings.get("USE_GOOGLE_SECRET_MANAGER"):
             self.settings.set("USE_GOOGLE_SECRET_MANAGER",
                               confirm_yesno("[BETA] Use Google Secret Manager to hold gluu configuration layer. "
@@ -43,25 +44,29 @@ class PromptGoogle:
         if self.settings.get("PERSISTENCE_BACKEND") == "spanner" or \
                 self.settings.get("USE_GOOGLE_SECRET_MANAGER") == "Y":
             if not self.settings.get("GOOGLE_SERVICE_ACCOUNT_BASE64"):
-                print("Place the google service account json file under the name google_service_account.json. at "
-                      "the same location as the installation script. The service account must have "
-                      "roles/secretmanager.admin to use Google secret manager and/or "
-                      "roles/spanner.databaseUser to use Spanner")
-                _ = input("Hit 'enter' or 'return' when ready.")
-                with open(Path("./google_service_account.json")) as content_file:
-                    sa = content_file.read()
-                    encoded_sa_crt_bytes = base64.b64encode(sa.encode("utf-8"))
-                    encoded_sa_crt_string = str(encoded_sa_crt_bytes, "utf-8")
-                self.settings.set("GOOGLE_SERVICE_ACCOUNT_BASE64", encoded_sa_crt_string)
-
-            try:
-                with open("google_service_account.json", "r") as google_sa:
-                    sa = json.load(google_sa)
-                    self.settings.set("GOOGLE_PROJECT_ID", sa["project_id"])
-            except FileNotFoundError:
-                print("The google service account json was not found. However, your setup has it included already in "
-                      "your settings.json.")
-                if not self.settings.get("GOOGLE_PROJECT_ID"):
-                    self.settings.set("GOOGLE_PROJECT_ID",
-                                      click.prompt("Please enter the google project ID",
-                                                   default=""))
+                try:
+                    print("Place the google service account json file under the name google_service_account.json. at "
+                          "the same location as the installation script. The service account must have "
+                          "roles/secretmanager.admin to use Google secret manager and/or "
+                          "roles/spanner.databaseUser to use Spanner")
+                    _ = input("Hit 'enter' or 'return' when ready.")
+                    with open(Path("./google_service_account.json")) as content_file:
+                        sa = content_file.read()
+                        encoded_sa_crt_bytes = base64.b64encode(sa.encode("utf-8"))
+                        encoded_sa_crt_string = str(encoded_sa_crt_bytes, "utf-8")
+                    self.settings.set("GOOGLE_SERVICE_ACCOUNT_BASE64", encoded_sa_crt_string)
+                except FileNotFoundError:
+                    print("The google service account json was not found."
+                          "your settings.json.")
+            if not self.settings.get("GOOGLE_PROJECT_ID"):
+                try:
+                    with open("google_service_account.json", "r") as google_sa:
+                        sa = json.load(google_sa)
+                        self.settings.set("GOOGLE_PROJECT_ID", sa["project_id"])
+                except FileNotFoundError:
+                    print("The google service account json was not found."
+                          "your settings.json.")
+                    if not self.settings.get("GOOGLE_PROJECT_ID"):
+                        self.settings.set("GOOGLE_PROJECT_ID",
+                                          click.prompt("Please enter the google project ID",
+                                                       default=""))
