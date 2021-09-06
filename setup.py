3217a73f2c87e888eb7c6a63243c61afc8967e2d
moabu <47318409+moabu@users.noreply.github.com>
2021-09-06 05:12:13 -0400

    fix: remove custom k8 package

--- setup.py
+++ setup.py
@@ -21,19 +21,11 @@ def find_version(*file_paths):
     raise RuntimeError("Unable to find version string.")
 
 
-def get_k8s_pkg():
-    # get local version (forked) kubernetes version
-    # see Local version segments https://www.python.org/dev/peps/pep-0440/#id43
-    here = os.path.abspath(os.path.dirname(__file__))
-    k8s_pkg = os.path.join(here, "pygluu", "kubernetes", "templates", "kubernetesv11.0.0.tar.gz")
-    return f"file:///{k8s_pkg}#egg=kubernetes-11.0.0"
-
-
 setup(
     name="pygluu-kubernetes",
     version=find_version("pygluu", "kubernetes", "version.py"),
     url="",
-    copyright="Copyright 2020, Gluu Cloud Native Edition",
+    copyright="Copyright 2021, Gluu Cloud Native Edition",
     license="Apache 2.0 <https://www.apache.org/licenses/LICENSE-2.0>",
     author="Gluu",
     author_email="mo@gluu.org",
@@ -47,20 +39,7 @@ setup(
         "ruamel.yaml>=0.16.5",
         "pyOpenSSL>=19.1.0",
         "cryptography>=2.8",
-        "pyDes>=2.0.0",  # TODO: Remove the following as soon as the update secret is moved to backend
-        "requests>=2.24.0",  # TODO: Remove the following as soon as the kubernetes python client is fixed upstream
-        "google-api-python-client>=1.8.0",  # TODO: Remove the following as soon as the kubernetes python client is fixed upstream
-        "requests_oauthlib>=1.3.0",  # TODO: Remove the following as soon as the kubernetes python client is fixed upstream
-        "certifi>=14.05.14",  # TODO: Remove the following as soon as the kubernetes python client is fixed upstream
-        "six>=1.9.0",  # TODO: Remove the following as soon as the kubernetes python client is fixed upstream
-        "python-dateutil>=2.5.3",  # TODO: Remove the following as soon as the kubernetes python client is fixed upstream
-        "setuptools>=46.1.3",  # TODO: Remove the following as soon as the kubernetes python client is fixed upstream
-        "pyyaml>=3.12",  # TODO: Remove the following as soon as the kubernetes python client is fixed upstream
-        "google-auth>=1.0.1",  # TODO: Remove the following as soon as the kubernetes python client is fixed upstream
-        "ipaddress>=1.0.17;python_version=='2.7'",  # TODO: Remove the following as soon as the kubernetes python client is fixed upstream
-        "websocket-client>=0.32.0,!=0.40.0,!=0.41.*,!=0.42.*",  # TODO: Remove the following as soon as the kubernetes python client is fixed upstream
-        "certifi>=2020.6.20",  # TODO: May be removed, required by requests package.
-        "kubernetes==11.0.0",
+        "kubernetes==18.20.0",
         "Flask==1.1.2",
         "Click!=7.0,>=6.7",
         "Flask-WTF >= 0.14.2",
@@ -72,9 +51,6 @@ setup(
         "PyYAML >= 5.4.1",
         "gunicorn >= 20.0.4",
         "gevent >= 20.9.0"
-    ],
-    dependency_links=[
-        get_k8s_pkg(),  # TODO: Remove the following as soon as the kubernetes python client is fixed upstream
     ],
     classifiers=[
         "Intended Audience :: Developers",
